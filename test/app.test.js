cb0443a9ffe16a21dc04b6c130efb6306646096a
GFuentesG <g7fuentes7@gmail.com>
2025-07-03 22:53:15 -0500

    install node

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,7 @@ describe('GET /', () => {
   it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
+    // expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
+    expect(res.text).toContain('Bootcamp de GitHub Action - Variables de Entorno');
   });
 });
